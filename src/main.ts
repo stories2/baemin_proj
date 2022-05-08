3c7e31b379aa6e6af640fc5c392d6462a5d276b4
Kim HyunWoo <stories2@naver.com>
2022-05-08 10:39:04 +0900

    asdfasdf

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import BootstrapVue3 from "bootstrap-vue-3";
 
 // Optional, since every component import their Bootstrap functionality
 // the following line is not necessary
-// import 'bootstrap'
+// import 'bootstrap' asdfasd
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
