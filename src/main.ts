bc1460d9877a997a4567a812d1ebf93984592aeb
Kim HyunWoo <stories2@naver.com>
2022-05-08 12:23:25 +0900

    sadfasdf

--- src/main.ts
+++ src/main.ts
@@ -12,4 +12,22 @@ import BootstrapVue3 from "bootstrap-vue-3";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsGtjCFm1CQv1u3lrd24eUf0FLrOJiPAA",
+  authDomain: "asdfadsfas.firebaseapp.com",
+  projectId: "asdfadsfas",
+  storageBucket: "asdfadsfas.appspot.com",
+  messagingSenderId: "901281032604",
+  appId: "1:901281032604:web:70bbc4bffb0e215bcf8059",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 createApp(App).use(router).use(BootstrapVue3).mount("#app");
