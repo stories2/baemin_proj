48c0e5c76638e97972c8e811552f5e1125ec832a
Kim HyunWoo <stories2@naver.com>
2022-05-22 02:13:32 +0900

    dasf

---
+++ src/components/TopBar.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="top-bar">
+    <b-row style="margin-bottom: 15px">
+      <b-col style="text-align: start">
+        <BIconBell />
+      </b-col>
+      <b-col> 방이동 <BIconArrowDownShort /></b-col>
+      <b-col style="text-align: end">
+        <BIconGrid />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col>
+        <b-input-group size="sm" class="mb-2">
+          <b-input-group-prepend is-text>
+            <BIconSearch />
+          </b-input-group-prepend>
+          <b-form-input type="search" placeholder="Search terms"></b-form-input>
+        </b-input-group>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "TopBar",
+});
+</script>
+
+<style scoped>
+.top-bar {
+  background-color: #2ac1bc;
+  height: 100px;
+  border-bottom-right-radius: 15px;
+  border-bottom-left-radius: 15px;
+  color: white;
+  padding: 15px;
+}
+</style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -1,29 +1,6 @@
 <template>
   <div>
-    <div class="top-bar">
-      <b-row style="margin-bottom: 15px">
-        <b-col style="text-align: start">
-          <BIconBell />
-        </b-col>
-        <b-col> 방이동 <BIconArrowDownShort /></b-col>
-        <b-col style="text-align: end">
-          <BIconGrid />
-        </b-col>
-      </b-row>
-      <b-row>
-        <b-col>
-          <b-input-group size="sm" class="mb-2">
-            <b-input-group-prepend is-text>
-              <BIconSearch />
-            </b-input-group-prepend>
-            <b-form-input
-              type="search"
-              placeholder="Search terms"
-            ></b-form-input>
-          </b-input-group>
-        </b-col>
-      </b-row>
-    </div>
+    <top-bar></top-bar>
     <div style="margin-top: 15px">
       <b-row>
         <template v-for="card in cardList" v-bind:key="card">
@@ -45,12 +22,14 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import CardButton from "../components/CardButton.vue";
+import TopBar from "../components/TopBar.vue";
 // import { BIconBatteryFull } from "bootstrap-icons-vue";
 
 export default defineComponent({
   components: {
     // BIconBatteryFull,
     CardButton,
+    TopBar,
   },
 
   data() {
@@ -120,13 +99,4 @@ export default defineComponent({
 });
 </script>
 
-<style scoped>
-.top-bar {
-  background-color: #2ac1bc;
-  height: 100px;
-  border-bottom-right-radius: 15px;
-  border-bottom-left-radius: 15px;
-  color: white;
-  padding: 15px;
-}
-</style>
+<style scoped></style>
