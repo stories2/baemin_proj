01f050f9d2db7cdfa81554f67d7522570b0825d9
Kim HyunWoo <stories2@naver.com>
2022-05-29 10:54:25 +0900

    asdfaf

---
+++ src/components/BasketItem.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <b-row style="margin-bottom: 15px">
+      <b-col>
+        <div>치즈카츠(첫주문추천메뉴)</div>
+      </b-col>
+      <b-col style="text-align: right">
+        <BIconXLg style="cursor: pointer" />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="2" md="4" lg="5">
+        <div class="food-img"></div>
+      </b-col>
+      <b-col>
+        <b-row>
+          <b-col>가격: 단품 (13,000원)</b-col>
+        </b-row>
+        <b-row>
+          <b-col style="font-weight: bold">13,000원</b-col>
+        </b-row>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BasketItem",
+});
+</script>
+
+<style scoped>
+div.food-img {
+  background-image: url("https://www.beginmate.com/Upload/TextEditor/microsoftgithublove.jpg");
+  width: 50px;
+  height: 50px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  border-radius: 8px;
+}
+
+@media (min-width: 768px) {
+  div.food-img {
+    width: 150px;
+    height: 150px;
+  }
+}
+</style>
--- src/views/BasketView.vue
+++ src/views/BasketView.vue
@@ -14,25 +14,24 @@
         </b-col>
       </b-row>
       <hr />
-      <b-row style="margin-bottom: 15px">
-        <b-col>
-          <div>치즈카츠(첫주문추천메뉴)</div>
-        </b-col>
-        <b-col>
-          <BIconXLg />
-        </b-col>
-      </b-row>
       <b-row>
-        <b-col cols="2">
-          <div class="food-img"></div>
+        <b-col cols="12" lg="6" style="margin-bottom: 15px">
+          <basket-item></basket-item>
         </b-col>
-        <b-col>
-          <b-row>
-            <b-col>가격: 단품 (13,000원)</b-col>
-          </b-row>
-          <b-row>
-            <b-col style="font-weight: bold">13,000원</b-col>
-          </b-row>
+        <b-col cols="12" lg="6">
+          <basket-item></basket-item>
+        </b-col>
+        <b-col cols="12" lg="6">
+          <basket-item></basket-item>
+        </b-col>
+        <b-col cols="12" lg="6">
+          <basket-item></basket-item>
+        </b-col>
+        <b-col cols="12" lg="6">
+          <basket-item></basket-item>
+        </b-col>
+        <b-col cols="12" lg="6">
+          <basket-item></basket-item>
         </b-col>
       </b-row>
     </section-card>
@@ -44,11 +43,13 @@ import { defineComponent } from "vue";
 import { useDispath, useSelector } from "../helpers";
 import InAppTopBar from "@/components/InAppTopBar.vue";
 import SectionCard from "@/components/SectionCard.vue";
+import BasketItem from "@/components/BasketItem.vue";
 
 export default defineComponent({
   components: {
     InAppTopBar,
     SectionCard,
+    BasketItem,
   },
   data() {
     return {
@@ -64,22 +65,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style scoped>
-div.food-img {
-  background-image: url("https://www.beginmate.com/Upload/TextEditor/microsoftgithublove.jpg");
-  width: 50px;
-  height: 50px;
-  background-position: center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  border-radius: 8px;
-}
-
-@media (min-width: 768px) {
-  div.food-img {
-    width: 150px;
-    height: 150px;
-  }
-}
-</style>
