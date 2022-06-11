edb647bf15985649373731d815438a5f1545ac56
Kim HyunWoo <stories2@naver.com>
2022-06-12 07:06:37 +0900

    asdf

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,7 @@
   <b-container class="bv-example-row">
     <router-view />
   </b-container>
+  <div ref="map" style="width: 500px; height: 400px"></div>
 </template>
 
 <script lang="ts">
@@ -26,6 +27,14 @@ export default defineComponent({
 
   mounted() {
     this.dispatch(initPayment({}));
+    var container = this.$refs.map; //지도를 담을 영역의 DOM 레퍼런스
+    var options = {
+      //지도를 생성할 때 필요한 기본 옵션
+      center: new (window as any).kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
+      level: 3, //지도의 레벨(확대, 축소 정도)
+    };
+
+    var map = new (window as any).kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
   },
 });
 </script>
