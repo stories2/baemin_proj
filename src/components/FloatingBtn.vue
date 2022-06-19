dbcfb199e39b66b0a7e1823b64a39bf595ed3802
Kim HyunWoo <stories2@naver.com>
2022-06-19 09:17:51 +0900

    Update FloatingBtn.vue

--- src/components/FloatingBtn.vue
+++ src/components/FloatingBtn.vue
@@ -1,19 +1,29 @@
 <template>
-  <div>
+  <div class="btn-container" @click="$router.push('/basket')">
     <BIconHandbag />
+    <b-badge variant="light" class="notify-cnt">
+      {{ orders.orderList.length }}
+    </b-badge>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { useDispath, useSelector } from "../helpers";
 
 export default defineComponent({
   name: "FloatingBtn",
+
+  data() {
+    return {
+      orders: useSelector((state) => state.orders),
+    };
+  },
 });
 </script>
 
 <style scoped>
-div {
+div.btn-container {
   position: fixed;
   right: 50px;
   bottom: 50px;
@@ -26,5 +36,13 @@ div {
   font-size: 30px;
   box-shadow: rgb(100 100 111 / 20%) 0px 7px 29px 0px;
   cursor: pointer;
+}
+
+.notify-cnt {
+  position: absolute;
+  transform: scale(0.75);
+  right: -8px;
+  top: -8px;
+  border-radius: 0.75em;
 }
 </style>
